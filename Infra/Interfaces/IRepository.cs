71643c9d05517fb2ff306d1c76ae428906037b94
joaosmendess <joaosilva0721@gamil.com>
2025-02-09 16:59:25 -0300

    D-001 create generic repository

---
+++ Infra/Interfaces/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Application.Infra.Interfaces
+{
+    public interface IRepository<T>
+    {
+        Task<List<T>> GetAllAsync();
+        Task<T> GetByIdAsync(string id);
+        Task AddAsync(T entity);
+        Task UpdateAsync(string id, T entity);
+        Task DeleteAsync(string id);
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,13 +1,29 @@
 using Application.Infra.Database;
 using Microsoft.OpenApi.Models;
 using Microsoft.Extensions.Options;
+using Application.Infra.Interfaces;
+using Application.Infra.Repositories;
+using MongoDB.Driver;
 
 var builder = WebApplication.CreateBuilder(args);
 
-
 builder.Services.Configure<MongoDbSettings>(
     builder.Configuration.GetSection("MongoDbSettings"));
-builder.Services.AddSingleton<MongoDbContext>();
+
+builder.Services.AddSingleton<IMongoClient, MongoClient>(sp =>
+{
+    var settings = sp.GetRequiredService<IOptions<MongoDbSettings>>().Value;
+    return new MongoClient(settings.ConnectionString);
+});
+
+builder.Services.AddScoped<IMongoDatabase>(sp =>
+{
+    var client = sp.GetRequiredService<IMongoClient>();
+    var settings = sp.GetRequiredService<IOptions<MongoDbSettings>>().Value;
+    return client.GetDatabase(settings.DatabaseName);
+});
+
+builder.Services.AddScoped(typeof(IRepository<>), typeof(GenericRepository<>));
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
@@ -33,7 +49,3 @@ app.MapControllers();
 
 app.Run();
 
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
